b12ab0f867917e4097b0021241572e802ef2290a
Markus Lorch <m.lorch@it-kult.de>
2012-08-15 01:16:40 +0200

    fix: headers for images

---
+++ classes/class.rex_com_mediaaccess_sendfile.inc.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* Plugin Media-Access - rex_com_mediaaccess class
+* @author m.lorch[at]it-kult[dot]de Markus Lorch
+* @author <a href="http://www.it-kult.de">www.it-kult.de</a>
+*/
+
+class rex_com_mediaaccess_sendfile
+{
+  var $filename;
+  var $filepath;
+  var $fullpath;
+  var $MEDIA;
+
+  function rex_com_mediaaccess_sendfile($oomedia)
+  {
+
+  }
+
+  function send()
+  {
+
+  }
+  
+  function getName()
+  {
+    
+  }
+
+  function getDescription()
+  {
+    
+  }
+  
+}
--- classes/extensions_sendfile/class.rex_com_mediaaccess_sendfile_readfile.inc.php
+++ classes/extensions_sendfile/class.rex_com_mediaaccess_sendfile_readfile.inc.php
@@ -16,19 +16,35 @@ class rex_com_mediaaccess_sendfile_readfile extends rex_com_mediaaccess_sendfile
     $this->filepath = $REX['MEDIAFOLDER'];
     $this->filename = $this->MEDIA->getFileName();
     $this->fullpath = $this->filepath.'/'.$this->filename;
+    $this->fileext = strtolower($this->MEDIA->getExtension());
   }
 
   function send()
   {
-    header("Pragma: public");
-    header("Expires: 0");
-    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-    header("Content-Type: application/force-download");
-    header("Content-Type: application/octet-stream");
-    header("Content-Type: application/download");
-    header("Content-Transfer-Encoding: binary");
-    header("Content-Length: ".$this->MEDIA->getSize());
-    header("Content-Disposition: attachment; filename=".$this->filename.";");
+    while(ob_get_level())
+      ob_end_clean();
+    
+    if(in_array($this->fileext,array('jpeg', 'jpg', 'png','gif')))
+    {
+      if($this->fileext == 'jpg')
+        $this->fileext = 'jpeg';
+      
+      header('Content-Disposition: inline; filename="'.$this->filename.'"');
+      header('Content-Type: image/'.$this->fileext);
+      header('Content-Length: '.$this->MEDIA->getSize());
+    }
+    else
+    {
+      header("Pragma: public");
+      header("Expires: 0");
+      header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+      header("Content-Type: application/force-download");
+      header("Content-Type: application/octet-stream");
+      header("Content-Type: application/download");
+      header("Content-Transfer-Encoding: binary");
+      header("Content-Length: ".$this->MEDIA->getSize());
+      header("Content-Disposition: attachment; filename=".$this->filename.";");
+    }
     
     @readfile($this->fullpath);
   }
